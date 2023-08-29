6bdf561802c3c8e97856de034a471d0912b3377f
Ba5t3t <lovely09lucky@gmail.com>
2023-08-29 19:42:40 +0300

    project complete

--- src/App.js
+++ src/App.js
@@ -14,17 +14,19 @@ import { Recipe } from './pages/Recipe';
 function App() {
   return (
     <>
-      <Router>
+      <Router basename='/react-food'>
         <Header />
         <main className="container content">
         
           <Switch>
-            <Route exact path="/" component={Home} />
+            <Route exact path='/'>
+              <Home />
+            </Route>
             <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/category/:meal" component={Category} />
-            <Route component={NotFound} />
+            <Route path="/contacts" component={Contact} />
+            <Route path="/category/:name" component={Category} />
             <Route path="/meal/:id" component={Recipe} />
+            <Route component={NotFound} />
           </Switch>
         
         </main>
